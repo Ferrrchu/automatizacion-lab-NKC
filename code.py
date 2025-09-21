8717bce3424cd9bb5cd560f02cbff7cbbd87bf67
Maria Fernanda <mariafernandasosa02@hotmail.com>
2025-09-20 21:35:58 -0300

    Actualiza code.py

--- code.py
+++ code.py
@@ -1,17 +1,37 @@
-import board
-import time
-import adafruit_dht
+import board # Carga el módulo de los nombres de los pines (GP0, GP1, etc.)
+import time # Carga el módulo de las pausas (sleep)
+import adafruit_dht # Carga el driver del sensor DHT (DHT11/DHT22)
+import digitalio
 
-# Ctrl + Shift + P para abrir la consola
-# Definir el tipo de sensor y el pin GPIO al que está conectado
-dht_device = adafruit_dht.DHT11(board.GP0)
+sensorTemperatura = adafruit_dht.DHT11(board.GP26) #Crea el objeto del sensor DHT11 indicando que su pin de datos está en GP0.
+
+sensorInfrarojo = digitalio.DigitalInOut(board.GP22)
+sensorInfrarojo.direction = digitalio.Direction.INPUT
+
+ledAzul = digitalio.DigitalInOut(board.GP14)   
+ledAzul.direction = digitalio.Direction.OUTPUT
+
+ledVerde = digitalio.DigitalInOut(board.GP13)   
+ledVerde.direction = digitalio.Direction.OUTPUT 
+
+ledBlanco = digitalio.DigitalInOut(board.GP12)   
+ledBlanco.direction = digitalio.Direction.OUTPUT 
+
+ledAmarillo = digitalio.DigitalInOut(board.GP11)   
+ledAmarillo.direction = digitalio.Direction.OUTPUT 
+
+ledRojo = digitalio.DigitalInOut(board.GP10)   
+ledRojo.direction = digitalio.Direction.OUTPUT 
+
+ledNaranja = digitalio.DigitalInOut(board.GP15)   
+ledNaranja.direction = digitalio.Direction.OUTPUT 
 
 # Bucle principal para leer los datos
 while True:
     try:
         # Intenta leer la temperatura y la humedad
-        temperatura = dht_device.temperature
-        humedad = dht_device.humidity
+        temperatura = sensorTemperatura.temperature #Lee la temperatura en °C
+        humedad = sensorTemperatura.humidity #Lee la humedad relativa en %
         
         # Imprimir los valores en la consola
         print(f"Temperatura: {temperatura}°C")
@@ -23,5 +43,14 @@ while True:
         time.sleep(2.0)
         continue
 
-    # Esperar 2 segundos antes de la siguiente lectura
+    valor = sensorInfrarojo.value
+    detectado = (valor == False)   # ajusta si tu módulo es al revés
+    if detectado:
+        print("Obstáculo DETECTADO")
+        ledNaranja.value = (sensorInfrarojo.value == False)
+        time.sleep(2.0)
+        ledNaranja.value = False
+        continue
+
+    # Esperar 2 segundos antes de la siguiente lectura, los DHT11 no deben leerse más de ~1 vez por segundo; 2 s es una práctica segura para lecturas estables.
     time.sleep(2.0)
